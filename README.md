2af4a0bdf4f0c5c4e486d63b670a7230885c0d40
Daniel Ngira <81624602+danielngira@users.noreply.github.com>
2024-12-23 15:39:21 +0530

    Made a game of rock, paper, scissors

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rock-paper-scissors
+A game of rock paper scissors using Vanilla JavaScript, HTML and CSS.
---
+++ javascript.js
@@ -0,0 +1,128 @@
+console.log("hello world!");
+
+let playerScore = 0;
+let computerScore = 0;
+const maxRounds = 5;
+
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
+
+function getComputerChoice() {
+    let choices = ["rock", "paper", "scissors"];
+    let choice = getRandomIntInclusive(0, 2)
+    return choices[choice];
+}
+
+function getHumanChoice() {
+    let choice = prompt("Move:");
+    console.log(`Player choice: ${choice}`);
+    return choice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    let lowerHumanChoice = humanChoice.toLowerCase();
+    let lowerComputerChoice = computerChoice.toLowerCase();
+    
+    console.log(`Computer choice: ${lowerComputerChoice}`);
+
+    switch (lowerHumanChoice) {
+        case "rock":
+            if (lowerComputerChoice == "paper"){
+                computerScore++;
+            }
+            else if (lowerComputerChoice == "scissors") {
+                playerScore++;
+            }
+            else if (lowerComputerChoice == "rock") {
+                console.log("You have tied, play again");
+            }
+            else {
+                throw new Error("Incorrect Computer input: Input should be either, rock, paper or scissors");
+            }
+            break;
+        case "paper":
+            if (lowerComputerChoice == "paper"){
+                console.log("You have tied, play again");
+            }
+            else if (lowerComputerChoice == "scissors") {
+                computerScore++;
+            }
+            else if (lowerComputerChoice == "rock") {
+                playerScore++;
+            }
+            else {
+                 throw new Error("Incorrect Computer input: Input should be either, rock, paper or scissors");
+            }
+            break;
+        case "scissors":
+            if (lowerComputerChoice == "paper"){
+                playerScore++;
+            }
+            else if (lowerComputerChoice == "scissors") {
+                console.log("You have tied, play again");
+            }
+            else if (lowerComputerChoice == "rock") {
+                computerScore++;
+            }
+            else {
+                 throw new Error("Incorrect Computer input: Input should be either, rock, paper or scissors");
+            }
+            break;
+        default:
+            throw new Error("Incorrect Player input: Input should be either, rock, paper or scissors");
+    }
+        console.log(`Scores => Player: ${playerScore}, Computer: ${computerScore}`);    
+}
+
+function restartGame(round, gameOver, winner) {
+    let restart = prompt("Do you want to restart the game? (yes or no)");
+    let response = restart.toLowerCase();
+    if (response === "yes") {
+        playerScore = 0;
+        computerScore = 0;
+        playgame()
+    }
+}
+
+function playgame() {
+    let round = 1;
+    let gameOver = false;
+    let winner = nul;
+
+    while (round <= maxRounds && !gameOver){
+       try{
+            let humanMove = getHumanChoice();
+            let computerMove = getComputerChoice();
+            playRound(humanMove, computerMove);
+            round++;
+        }
+        catch(e) {  
+            console.error(e);
+        }
+        if (playerScore >= 3) {
+            winner = "player";
+            gameOver = true;
+        }
+        else if (computerScore >= 3){
+            winner = "computer";
+            gameOver = true;
+        }
+        else if (round > maxRounds && (computerScore == playerScore || computerScore < 3 && playerScore < 3)) {
+            console.log("The game has ended in a tie");
+            gameOver = true;
+        }
+    }
+
+    if (winner) {
+        console.log(`The winner is: ${winner}`);
+    } else {
+        console.log("The game is a tie!");
+    }
+
+    restartGame();
+}
+
+playgame();
